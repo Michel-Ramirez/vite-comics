f24d56b56089d4ad095592dd2f58e151d83b1be9
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-29 15:38:43 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+  </div>
+</template>
+
+<style></style>
